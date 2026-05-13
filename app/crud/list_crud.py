bf1ab600a544175c59e9633dfff9223078be1f5d
TT-RR <kaiza1mage@gmail.com>
2026-05-13 12:20:09 +0900

    create list_crud

---
+++ app/crud/list_crud.py
@@ -0,0 +1,41 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from app.models.list_model import ListModel
+from app.schemas.list_schema import NewTodoList, UpdateTodoList, ResponseTodoList
+
+def get_todo_list(todo_list_id: int, db: Session):
+    return db.query(ListModel).filter(ListModel.id == todo_list_id).first()
+
+def post_todo_list(new_data: NewTodoList, db: Session):
+	post_date = ListModel(
+        title=new_data.title,
+        description=new_data.description
+    )
+	db.add(post_date)
+	db.commit()
+	db.refresh(post_date)
+	return post_date
+
+def put_todo_list(todo_list_id: int, data: UpdateTodoList, db: Session):
+	update_data = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
+	if not update_data:
+		raise HTTPException(status_code=404, detail="指定のIDのデータは見つかりませんでした")
+	
+	if data.title:
+		update_data.title = data.title
+	if data.description:
+		update_data.description = data.description
+	db.add(update_data)
+	db.commit()
+	db.refresh(update_data)
+	return update_data
+
+def delete_todo_list(todo_list_id: int, db: Session):
+	db_item = db.get(ListModel, todo_list_id)
+	if not db_item:
+		return False
+
+	db.delete(db_item)
+	db.commit()
+	return True
--- app/routers/list_router.py
+++ app/routers/list_router.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.dependencies import get_db
 from app.models.list_model import ListModel
+from app.crud import list_crud
 from app.schemas.list_schema import NewTodoList, UpdateTodoList, ResponseTodoList
 
 router = APIRouter(
@@ -14,44 +15,27 @@ router = APIRouter(
 # Station6	Todoリスト取得
 @router.get("/lists/{todo_list_id}", tags=["Todoリスト"])
 def get_todo_list(todo_list_id: int, db: Session = Depends(get_db)):
-    db_item = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
+    db_item = list_crud.get_todo_list(todo_list_id, db)
     return db_item
 
 # Station7	Todoリスト新規作成
 @router.post("/lists", tags=["Todoリスト"])
 def post_todo_list(new_data: NewTodoList, db: Session = Depends(get_db)):
-    post_date = ListModel(
-        title=new_data.title,
-        description=new_data.description
-    )
-    db.add(post_date)
-    db.commit()
-    db.refresh(post_date)
+    post_date = list_crud.post_todo_list(new_data, db)
     return post_date
 
 # Station8	Todoリスト更新
 @router.put("/lists/{todo_list_id}", tags=["Todoリスト"], response_model=ResponseTodoList)
 def put_todo_list(todo_list_id: int, data: UpdateTodoList, db: Session = Depends(get_db)):
-    update_data = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
-    if not update_data:
-        raise HTTPException(status_code=404, detail="指定のIDのデータは見つかりませんでした")
-
-    if data.title:
-        update_data.title = data.title
-    if data.description:
-        update_data.description = data.description
-    db.add(update_data)
-    db.commit()
-    db.refresh(update_data)
+    update_data = list_crud.put_todo_list(todo_list_id, data, db)
     return update_data
 
 # Station9	Todoリスト削除
 @router.delete("/lists/{todo_list_id}", tags=["Todoリスト"])
 def delete_todo_list(todo_list_id: int, db: Session = Depends(get_db)):
-    db_item = db.get(ListModel, todo_list_id)
-    if not db_item:
-        raise HTTPException(status_code=404, detail="Todo not found")
-
-    db.delete(db_item)
-    db.commit()
-    return {}
+	item_flag = list_crud.delete_todo_list(todo_list_id, db)
+	
+	if item_flag == False:
+		HTTPException(status_code=404, detail="Todo not found")
+	return {}
+		
