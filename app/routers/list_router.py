2faa857b9b1cb9e5954a4416d34e6ec1b16dbcd2
TT-RR <kaiza1mage@gmail.com>
2026-05-13 11:53:16 +0900

    TODOリスト用のリクエストボディなどの型定義を移動

--- app/routers/list_router.py
+++ app/routers/list_router.py
@@ -1,42 +1,17 @@
 import os
-from datetime import datetime
 
 from fastapi import FastAPI
 from fastapi import APIRouter, HTTPException, Depends
-from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
-from app.const import TodoItemStatusCode
-
 from app.dependencies import get_db
-from app.models.item_model import ItemModel
 from app.models.list_model import ListModel
+from app.schemas.list_schema import NewTodoList, UpdateTodoList, ResponseTodoList
 
 router = APIRouter(
 	prefix="/lists",
 	tags=["TODOリスト"],
 )
-
-class NewTodoList(BaseModel):
-    """TODOリスト新規作成時のスキーマ."""
-
-    title: str = Field(title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
-
-class UpdateTodoList(BaseModel):
-    """TODOリスト更新時のスキーマ."""
-
-    title: str | None = Field(default=None, title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
-
-class ResponseTodoList(BaseModel):
-    """TODOリストのレスポンススキーマ."""
-
-    id: int
-    title: str = Field(title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
-    created_at: datetime = Field(title="datetime that the item was created")
-    updated_at: datetime = Field(title="datetime that the item was updated")
 
 # Station6	Todoリスト取得
 @router.get("/lists/{todo_list_id}", tags=["Todoリスト"])
